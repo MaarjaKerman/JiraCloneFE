db760cfb5fc75c5d8acfc24b671ad4d2b25b19e4
Kristofer Jermolov <kristofer.jermolov@gmail.com>
2022-11-15 10:05:53 +0200

    Workshop 18 practice

---
+++ cypress/e2e/workshop-18.cy.js
@@ -0,0 +1,9 @@
+/**
+ * Workshop 18 practical tasks:
+ * 1. Make full page screenshot using chrome devTools
+ * 2. For test 'Should create an issue and validate it successfully' in issue-create-2.cy.js
+ * log as table all innerText values for '[data-testid="list-issue"]' (hint: user .then just after cy.get)
+ * 3. get with JQuery first element with '[data-testid="list-issue"]' locator and its HTML
+ * 4. get with JQuery all elements with '[data-testid="list-issue"]' locator
+ * 5. More advanced JQuery topic - get all innerText values for all '[data-testid="list-issue"]' elements (hint: use .map and ...)
+ */
