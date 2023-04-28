d2d66c2b202f1f35687055efe5c339b26ab97a79
nadezda.matvejeva <nadezda.matvejeva@tripledev.ee>
2023-04-28 16:24:30 +0300

    Code fixed

---
+++ cypress.config.js
@@ -0,0 +1,15 @@
+const { defineConfig } = require("cypress");
+
+module.exports = defineConfig({
+  e2e: {
+    setupNodeEvents(on, config) {
+      // implement node event listeners here
+    },
+    baseUrl: 'https://jira.ivorreic.com/project/board',
+    env: {
+      baseUrl: 'https://jira.ivorreic.com/',
+    },
+    defaultCommandTimeout: 30000,
+    requestTimeout: 15000,
+  },
+});
