c8fe855e3a274aff49a038cdf3b8e558199ece27
nadezda.matvejeva <nadezda.matvejeva@tripledev.ee>
2023-04-28 17:14:45 +0300

    Code updated

--- cypress/e2e/issue-details-edit.cy.js
+++ cypress/e2e/issue-details-edit.cy.js
@@ -1,36 +1,3 @@
-/**
- * Workshop #15
- * 1. Start creating classes for testable objects, such as "Issue"
- * 2. Move test actions into functions as we did on demo previously
- *    2.1 Update issue type, description etc.
- * 3. Use those functions in the spec file
- *
- * Expected result:
- * 1. New class with methods for updating title, description etc.
- * 2. Variables correctly stored
- *    Bonus: used random data generator library (faker.js)
- */
-
-/**
- * Workshop #16
- * Task #1
- * 1. Look for previously created method for validating information in the field (any field)
- *    1.1 One of them was: cy.get('[data-testid="select:priority"]').should('have.text', 'Medium');
- * 2. Define an object with expected information in the style: selector/it's value
- * 3. Update method to go over this object and assert information in the field using "for..of" loop
- *
- * Expected result:
- * 1. You will have defined object with at least priority, status and reporter selectors and their values
- * 2. Your method runs X amount of times and assert information in the field without code duplication
- *
- * Task #2
- * Most of the field in this file are using should('have.text') assertion, however, there are some which uses should('contain')
- * From the previous task expand the solution with "if" check which would allow us to assert using different should assertion but still keep all the code inside one loop without creating separated assertion
- *
- * Expected result:
- * 1. Previously created method will have more selectors included in the object (for example, assignees are added)
- */
-
 describe('Issue details editing', () => {
   beforeEach(() => {
     cy.visit('/');
@@ -92,21 +59,6 @@ describe('Issue details editing', () => {
       cy.get('textarea[placeholder="Short summary"]').should('have.text', title);
       cy.get('.ql-snow').should('have.text', description);
     });
-  });
-
-  it('Should delete an issue successfully', () => {
-    getIssueDetailsModal()
-      .find(`button ${'[data-testid="icon:trash"]'}`)
-      .click();
-
-    cy.get('[data-testid="modal:confirm"]')
-      .contains('button', 'Delete issue')
-      .click();
-    cy.get('[data-testid="modal:confirm"]')
-        .should('not.exist');
-
-    cy.contains('This is an issue of type: Task.').should('not.exist');
-
   });
 
   const getIssueDetailsModal = () => cy.get('[data-testid="modal:issue-details"]');
