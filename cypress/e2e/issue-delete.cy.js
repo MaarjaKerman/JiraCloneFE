db95012ac6386d4def9fe797d12292d0e86a4957
MaarjaKerman <Maarja409@gmail.com>
2024-05-29 22:42:39 +0800

    My commets about the commit

--- cypress/e2e/issue-delete.cy.js
+++ cypress/e2e/issue-delete.cy.js
@@ -1,36 +1,51 @@
-describe('Issue delete', () => {
+describe("Issue deletion", () => {
+  const getIssueDetailsModal = () =>
+    cy.get('[data-testid="modal:issue-details"]');
+
   beforeEach(() => {
-    cy.visit('/');
-    cy.url().should('eq', `${Cypress.env('baseUrl')}project`).then((url) => {
-    cy.visit(url + '/board');
-    cy.contains("This is an issue of type: Task.").click();
+    cy.visit("/");
+    cy.url()
+      .should("eq", `${Cypress.env("baseUrl")}project`)
+      .then((url) => {
+        cy.visit(url + "/board");
+        cy.contains("This is an issue of type: Task.").click();
+      });
+  });
+
+  // Assignment 3: Test case 1 //
+  it("Should delete an issue successfully", () => {
+    getIssueDetailsModal().within(() => {
+      cy.get('[data-testid="icon:trash"]').click();
+    });
+    cy.get('[data-testid="modal:confirm"]').should("be.visible");
+    cy.get('[data-testid="modal:confirm"]').within(() => {
+      cy.get("button").contains("Delete issue").click();
+    });
+
+    cy.get('[data-testid="modal:confirm"]').should("not.exist");
+    cy.get('[data-testid="modal:issue-details"]').should("not.exist");
+    cy.get('[data-testid="board-list:backlog"]').within(() => {
+      cy.contains("This is an issue of type: Task.").should("not.exist");
+      cy.get('[data-testid="list-issue"]').should("have.length", 3);
     });
   });
 
-const getIssueDetailsModal = () => cy.get('[data-testid="modal:issue-details"]');
-
-it('Should delete an issue successfully', () => {
+  // Assignment 3: Test case 2 //
+  it("Should initiate the deletion and then cancel it successfully", () => {
     getIssueDetailsModal().within(() => {
-cy.get('[data-testid="icon:trash"]').click();
-});
-cy.get('[data-testid="modal:confirm"]').within(() => {
-cy.get("button").contains("Delete issue").click();
-});
-cy.get('[data-testid="modal:confirm"]').should("not.exist")
-cy.contains("This is an issue of type: Task.").should("not.exist")
- });
+      cy.get('[data-testid="icon:trash"]').click();
+    });
+    cy.get('[data-testid="modal:confirm"]').should("be.visible");
+    cy.get('[data-testid="modal:confirm"]').within(() => {
+      cy.get("button").contains("Cancel").click();
+    });
 
-it('Should initiate the deletion and then cancel it successfully', () => {
-    getIssueDetailsModal().within((t) => {
-cy.get('[data-testid="icon:trash"]').click();
+    cy.get('[data-testid="modal:confirm"]').should("not.exist");
+    cy.get('[data-testid="modal:issue-details"]').should("be.visible");
+    cy.get('[data-testid="icon:close"]').eq(0).click();
+    cy.get('[data-testid="board-list:backlog"]').within(() => {
+      cy.contains("This is an issue of type: Task.").should("be.visible");
+      cy.get('[data-testid="list-issue"]').should("have.length", 4);
+    });
+  });
 });
-cy.get('[data-testid="modal:confirm"]').within(() => {
-cy.get("button").contains("Cancel").click();
-});
-cy.get('[data-testid="modal:confirm"]').should("not.exist")
-cy.get('[data-testid="icon:close"]').eq(0).click();
-cy.contains("This is an issue of type: Task.").should("be.visible")
-
-});
-});
-
